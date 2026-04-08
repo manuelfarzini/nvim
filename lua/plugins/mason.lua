5e18cc1e3a36cd44e0103ac097a9bef698391532
Manuel Farzini <manuel.farzini@gmail.com>
2026-04-08 17:29:45 +0200

    Periodic update.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,65 +1,62 @@
 ---@diagnostic disable: undefined-global
 return {
-  {
-
-    "mason-org/mason.nvim",
-    build = ":MasonUpdate",
-    dependencies = {
-      "mason-org/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      "neovim/nvim-lspconfig",
-    },
-
-    config = function()
-      require("mason").setup({
-        ui = {
-          border = "rounded",
-          backdrop = 100,
-          width = 0.85,
-          height = 0.95,
-          icons = {
-            package_installed = "",
-            package_pending = "",
-            package_uninstalled = "",
-          },
-        },
-      })
-
-  ---@diagnostic disable-next-line: missing-fields
-  require("mason-lspconfig").setup({
-    ensure_installed = {
-      "clangd",
-      "bashls",
-      "lua_ls",
-      "rust_analyzer",
-      "ols",
-      "gopls",
-      "zls",
-      "matlab_ls",
-      -- "jdtls",
-      "pyright",
-      -- "html",
-      -- "cssls",
-      -- "intelephense",
-      -- "graphql",
-    },
-    automatic_installation = true,
-    automatic_setup = false,
-  })
-
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          "isort",
-          "markdownlint",
-          "phpcbf",
-          "prettier",
-          "stylua",
-          -- "eslint_d",
-          -- "codelldb",
-        },
-        auto_update = true,
-        run_on_start = true,
-      })
-    end,
+  "mason-org/mason.nvim",
+  build = ":MasonUpdate",
+  dependencies = {
+    "mason-org/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "neovim/nvim-lspconfig",
   },
+
+  config = function()
+    require("mason").setup({
+      ui = {
+        border = "rounded",
+        backdrop = 100,
+        width = 0.85,
+        height = 0.95,
+        icons = {
+          package_installed = "",
+          package_pending = "",
+          package_uninstalled = "",
+        },
+      },
+    })
+
+    ---@diagnostic disable-next-line: missing-fields
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "clangd",
+        "bashls",
+        "lua_ls",
+        "rust_analyzer",
+        "ols",
+        "gopls",
+        "zls",
+        "matlab_ls",
+        -- "jdtls",
+        "pyright",
+        -- "html",
+        -- "cssls",
+        -- "intelephense",
+        -- "graphql",
+      },
+      automatic_installation = true,
+      automatic_setup = false,
+    })
+
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "isort",
+        "markdownlint",
+        "phpcbf",
+        "prettier",
+        "stylua",
+        -- "eslint_d",
+        -- "codelldb",
+      },
+      auto_update = true,
+      run_on_start = true,
+    })
+  end,
 }
